9d13f0ed9ae249731f27e842632f576540d26d0f
LT <leetoplayaz@yandex.ru>
2020-11-27 18:48:43 +0300

    cashItems

--- unitpay/modules/payment/backend/unitpay/payment.unitpay.view.php
+++ unitpay/modules/payment/backend/unitpay/payment.unitpay.view.php
@@ -15,8 +15,11 @@ if (! defined('DIAFAN'))
 	?>
 	<form name="unitpay" action="https://<?php echo $result["domain"]; ?>/pay/<?php echo $result["public_key"]; ?>">
 		<input type="hidden" name="account" value="<?php echo $result["account"]; ?>">
-		<input type="hidden" name="sum" value=" <?php echo $result["sum"]; ?>">
+		<input type="hidden" name="sum" value="<?php echo $result["sum"]; ?>">
 		<input type="hidden" name="desc" value="<?php echo $result["desc"]; ?>">
+		<input type="hidden" name="signature" value="<?php echo $result['signature']; ?>">
+		<input type="hidden" name="currency" value="<?php echo $result['currency']; ?>">
+		<input type="hidden" name="customerPhone" value="<?php echo $result['customerPhone']; ?>">
 		<input type="hidden" name="customerEmail" value="<?php echo $result['customerEmail']; ?>">
 		<?php if ($result['showCashItems']): ?>
 		<input type="hidden" name="cashItems" value="<?php echo $result['cashItems']; ?>">
